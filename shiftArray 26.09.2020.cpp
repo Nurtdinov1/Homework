df7357c3014f01f78b374753720141c097eace6e
Nurtdinov1 <72566241+Nurtdinov1@users.noreply.github.com>
2021-02-06 20:18:15 +0300

    Add files via upload

---
+++ shiftArray 26.09.2020.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+void reversArray(int N[], int n) {
+    for (int l = 0, r = n - 1; l < r; ++l, --r) {
+         int tmp = N[l]; N[l] = N[r]; N[r] = tmp;
+    }
+}
+
+void shiftArray(int N[], int n, int k) {
+    while (k < 0) {
+        k += n;
+    }
+    while (k >= n) {
+        k -= n;
+    }
+    if (k > 0) {
+        reversArray(N, k);
+        reversArray(N + k, n - k);
+        reversArray(N, n);
+    }
+}
+
+void printArray(const int N[], int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << N[i] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int n = 20, m = 5;
+    int* M = new int[n];
+    
+    for (int i = 0; i < n; ++i) {
+        M[i] = i + 1;
+    }
+    
+    shiftArray(M, n, m);
+    printArray(M, n);
+    delete[] M;
+    
+    return 0;
+}
---
+++ snakeArray 26.09.2020.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <iomanip>
+
+void snake(int** N, int n) {
+	int a = 1, b = 0, c = 0, d = 1;
+	while (a <= n * (n + 1) / 2) {
+		N[b][c] = a++;
+		b += d;
+		c -= d;
+		if (c < 0) {
+			c = 0;
+			d = -d;
+		}
+		if (b < 0) {
+			b = 0;
+			d = -d;
+		}
+	}
+	b += d;
+	c -= d;
+	
+	while (a <= n * n) {
+		N[b][c] = a++;
+		b += d;
+		c -= d;
+		if (b >= n) {
+			d = -d;
+			b = n - 1;
+			c += 2;
+		}if (c >= n) {
+			d = -d;
+			c = n - 1;
+			b += 2;
+		}
+	}
+}
+
+int main()
+{
+	int n = 10;
+	int** N = new int* [n];
+	for (int i = 0; i < n; ++i) {
+		N[i] = new int[n];
+	}
+	snake(N, n);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			std::cout << std::setw(4) << N[i][j];
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < n; ++i)
+		delete[] N[i];
+	delete[] N;
+	return 0;
+}
