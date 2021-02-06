30a9d5383a51d2b3e4aa26f7cb36a1ef718de569
Nurtdinov1 <72566241+Nurtdinov1@users.noreply.github.com>
2021-02-06 20:35:34 +0300

    Add files via upload

---
+++ fsolve 3.10.2020.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+double fx(double x) {
+	return 2 * x * x * x - 15 * x * x + 9 * x - 17;
+}
+double derivative(double x) {
+	return 6 * x * x - 30 * x + 9;
+}
+typedef double(*fun)(double x);
+double fsolve(fun fx, fun derivative, double x_0) {
+	double x_1 = x_0 - fx(x_0) / derivative(x_0);
+	while (fabs(x_1 - x_0) > 0.000001) {
+		x_0 = x_1;
+		x_1 = x_0 - fx(x_0) / derivative(x_0);
+	}
+	return x_1;
+}
+
+int main() {
+	std::cout << fsolve(fx, derivative, 10);
+	return 0;
+}
---
+++ srednee 3.10.2020.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdarg.h>
+
+double average(double x, ...) {
+	va_list args;
+	va_start(args, x);
+	double c = x, sr = 0;
+	int n = 0;
+	do {
+		sr += c;
+		++n;
+		c = va_arg(args, double);
+	} while (c != -1.);
+	va_end(args);
+	sr /= n;
+	return sr;
+}
+
+int main()
+{
+	std::cout << average(1., 2., 3., 4., 5., -1.) << std::endl;
+}
