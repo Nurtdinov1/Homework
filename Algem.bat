7b592abd4b052f3dec74401926a58266196bd7b0
Nurtdinov1 <72566241+Nurtdinov1@users.noreply.github.com>
2021-02-08 18:31:45 +0300

    Add files via upload

---
+++ Algem.bat
@@ -0,0 +1,5 @@
+chcp 1251
+@echo off
+cd "1 курс"\"1 семестр"\"Алгебра и геометрия"
+ehco > %DATE%.txt
+type con > %DATE%.txt
---
+++ OP.bat
@@ -0,0 +1,5 @@
+chcp 1251
+@echo off
+cd "1 курс"\"1 семестр"\"Основы программирования"
+ehco > %DATE%.txt
+type con > %DATE%.txt
